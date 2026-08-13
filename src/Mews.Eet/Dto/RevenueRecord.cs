a395dd2fefb12807092a492e33bd92dc8fe074fc
Samuel Kabourek <kabourek@e-apollo.cz>
2026-08-13 08:33:43 +0200

    feat: remove generation of PKP(Signature) and BKP(SecurityCode)

--- src/Mews.Eet/Dto/RevenueRecord.cs
+++ src/Mews.Eet/Dto/RevenueRecord.cs
@@ -7,7 +7,7 @@ namespace Mews.Eet.Dto
 {
     public class RevenueRecord
     {
-        public RevenueRecord(Identification identification, Revenue revenue, BillNumber billNumber, bool mandatedByMultipleTaxPayers = false, bool isFirstAttempt = true, EvidenceMode mode = EvidenceMode.Online)
+        public RevenueRecord(Identification identification, Revenue revenue, BillNumber billNumber, bool mandatedByMultipleTaxPayers = false, bool isFirstAttempt = true)
         {
             Identifier = Guid.NewGuid();
             Identification = identification;
@@ -15,9 +15,6 @@ namespace Mews.Eet.Dto
             Revenue = revenue;
             BillNumber = billNumber;
             IsFirstAttempt = isFirstAttempt;
-            Mode = mode;
-            Signature = Convert.ToBase64String(GetSignatureBytes());
-            SecurityCode = GetSecurityCode();
         }
 
         public Guid Identifier { get; }
@@ -31,27 +28,5 @@ namespace Mews.Eet.Dto
         public BillNumber BillNumber { get; }
 
         public bool IsFirstAttempt { get; }
-
-        public EvidenceMode Mode { get; }
-
-        public string Signature { get; }
-
-        public string SecurityCode { get; }
-
-        private string GetSecurityCode()
-        {
-            var hash = new SHA1Managed().ComputeHash(GetSignatureBytes());
-            var stringHash = StringHelpers.TransformToBase16(hash);
-            return StringHelpers.FormatOctets(stringHash);
-        }
-
-        private byte[] GetSignatureBytes()
-        {
-            var content = $"{Identification.TaxPayerIdentifier.Value}|{Identification.RegistrationUnitIdentifier.Value}|{Identification.RegistryIdentifier.Value}|{BillNumber.Value}|{StringHelpers.FormatForEet(Revenue.Accepted)}|{StringHelpers.FormatForEet(Revenue.Gross.Value)}";
-            var hash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(content));
-            var formatter = new RSAPKCS1SignatureFormatter(Identification.Certificate.Key);
-            formatter.SetHashAlgorithm("SHA256");
-            return formatter.CreateSignature(hash);
-        }
     }
 }
